21aecb2d1933515b4d03731fb322dcfca2e7a973
LAPTOP-LTAPOCEJ\Irawan Papang S <irawanpapangsubakti28@gmail.com>
2021-10-19 06:32:40 +0700

    1. jar

--- tugas_akhir_johan.sql
+++ tugas_akhir_johan.sql
@@ -11,10 +11,28 @@
  Target Server Version : 100421
  File Encoding         : 65001
 
- Date: 18/10/2021 15:59:39
+ Date: 19/10/2021 06:32:04
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for kriteria
+-- ----------------------------
+DROP TABLE IF EXISTS `kriteria`;
+CREATE TABLE `kriteria`  (
+  `seq` int NOT NULL AUTO_INCREMENT,
+  `kriteria_name` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`seq`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = COMPACT;
+
+-- ----------------------------
+-- Records of kriteria
+-- ----------------------------
+INSERT INTO `kriteria` VALUES (1, 'Promo');
+INSERT INTO `kriteria` VALUES (2, 'Metode Pembayaran');
+INSERT INTO `kriteria` VALUES (3, 'Metode Pengiriman');
+INSERT INTO `kriteria` VALUES (4, 'Layanan CS');
 
 SET FOREIGN_KEY_CHECKS = 1;
