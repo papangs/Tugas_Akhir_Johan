e5f0b5e3e14385b8cab772f2058270dacd095b33
LAPTOP-LTAPOCEJ\Irawan Papang S <irawanpapangsubakti28@gmail.com>
2021-10-18 16:00:05 +0700

    1. database

---
+++ tugas_akhir_johan.sql
@@ -0,0 +1,20 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : Mysql
+ Source Server Type    : MySQL
+ Source Server Version : 100421
+ Source Host           : localhost:3306
+ Source Schema         : tugas_akhir_johan
+
+ Target Server Type    : MySQL
+ Target Server Version : 100421
+ File Encoding         : 65001
+
+ Date: 18/10/2021 15:59:39
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+SET FOREIGN_KEY_CHECKS = 1;
